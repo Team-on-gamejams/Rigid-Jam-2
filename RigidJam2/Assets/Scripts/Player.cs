a12a481566d603de0f9dad6be2dbdc2e14636b34
Team-on <timonsol13@gmail.com>
2020-03-24 10:26:00 +0200

    Init

---
+++ RigidJam2/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+using TMPro;
+
+public class Player : MonoBehaviour {
+	public static Player instance;
+
+	private void Awake() {
+		instance = this;
+	}
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			Application.LoadLevel(0);
+		}
+		else if (Input.GetKeyDown(KeyCode.R)) {
+			Application.Quit();
+		}
+	}
+}
