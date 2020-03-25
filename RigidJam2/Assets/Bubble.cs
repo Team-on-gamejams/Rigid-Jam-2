231d0843835239a7c41f597b031722de6acbd762
Team-on <timonsol13@gmail.com>
2020-03-25 03:37:00 +0200

    Rigidjam entry

---
+++ RigidJam2/Assets/Bubble.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour {
+	public bool isPlaced = false;
+	bool isJoin = false;
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		if (isJoin)
+			return;
+
+		if(collision.transform.tag == "Bubble") {
+			if(!isPlaced)
+				Player.instance.NeedNew();
+
+			FixedJoint2D joint = gameObject.AddComponent<FixedJoint2D>();
+			joint.connectedBody = collision.gameObject.GetComponent<Rigidbody2D>();
+			isJoin = true;
+		}
+	}
+}
--- RigidJam2/Assets/Scripts/Player.cs
+++ RigidJam2/Assets/Scripts/Player.cs
@@ -9,8 +9,28 @@ using TMPro;
 public class Player : MonoBehaviour {
 	public static Player instance;
 
+	public GameObject goPrefab;
+	public GameObject currGo;
+	public CinemachineVirtualCamera cam;
+
+	List<Vector3> prevMousePos = new List<Vector3>(30);
+	float posDelta = 0.0f;
+
+	Camera main;
+
+	public void NeedNew() {
+		currGo.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+
+			currGo = Instantiate(goPrefab, currGo.transform.position + Vector3.up * 5, Quaternion.identity);
+		transform.SetParent(currGo.transform, false);
+		currGo.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+	}
+
 	private void Awake() {
 		instance = this;
+		main = Camera.main;
+		Cursor.lockState = CursorLockMode.Locked;
+		transform.SetParent(currGo.transform, false);
 	}
 
 	private void Update() {
@@ -20,5 +40,13 @@ public class Player : MonoBehaviour {
 		else if (Input.GetKeyDown(KeyCode.R)) {
 			Application.Quit();
 		}
+
+		Vector3 mouseMove = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+		if(mouseMove.magnitude >= 0.1f) {
+			currGo.transform.position += mouseMove * Time.deltaTime * 75;
+		}
+
+		if (mouseMove.magnitude >= 0.01f)
+			cam.m_Lens.OrthographicSize += Time.deltaTime * 6;
 	}
 }
